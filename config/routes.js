9b38f52a517776e3f7a03e64558d03190f6a0417
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-14 08:50:20 -0700

    user router set up

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,7 @@
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const axios = require('axios');
 
 const { authenticate } = require('../auth/authenticate');
@@ -10,6 +14,21 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => {
+    const token = generateToken(user);
+
+    res.status(201).json({ token });
+  })
+  .catch(error => {
+    res.status(500).json(error.message)
+  })
 }
 
 function login(req, res) {
